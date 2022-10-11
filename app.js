a97d5a3f6dab52331da4c84a4670c1a88220dd3b
vKocmac <vkocmac@gmail.com>
2022-10-11 21:38:27 +0300

    Make first 16x16 grid

---
+++ app.js
@@ -0,0 +1,16 @@
+
+//create div with class container
+const body = document.querySelector("body");
+const container = document.createElement("div");
+container.classList.add("container");
+body.appendChild(container);
+
+// const sqr = document.createElement("div");
+// sqr.classList.add("square");
+// container.appendChild(sqr);
+const sqr = [];
+for (let i=0; i<256;i++){
+  sqr[i] = document.createElement("div");
+  sqr[i].classList.add("square");
+  container.appendChild(sqr[i]);
+}
