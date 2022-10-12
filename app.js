fb7c61505a232992eaddebf9778a0196a4da6d80
vKocmac <vkocmac@gmail.com>
2022-10-12 20:17:32 +0300

    Add button that changes grid size

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
+let side = 16;
+let size = 256;
+let body;
+let container;
 
-//create div with class container
-const body = document.querySelector("body");
-const container = document.createElement("div");
-container.classList.add("container");
-body.appendChild(container);
+const button = document.querySelector("button");
+button.addEventListener("click", (e)=> {
+   side = prompt("Select Grid Size");
+   size = side*side;
+   body.removeChild(container);
+   canvas();
+})
+    function canvas(){
+  //create div with class container
+   body = document.querySelector("body");
+   container = document.createElement("div");
+  container.classList.add("container");
 
-// const sqr = document.createElement("div");
-// sqr.classList.add("square");
-// container.appendChild(sqr);
-const sqr = [];
-for (let i=0; i<256;i++){
-  sqr[i] = document.createElement("div");
-  sqr[i].classList.add("square");
-  container.appendChild(sqr[i]);
+  body.appendChild(container);
+
+  //create squares inside
+
+  const sqr = [];
+  for (let i=0; i<size;i++){
+    sqr[i] = document.createElement("div");
+    sqr[i].classList.add("square");
+    sqr[i].style.width = 800/side+"px";
+    sqr[i].style.height = 800/side+"px";
+    container.appendChild(sqr[i]);
+  }
+
+  //permament hover
+  sqr.forEach((item) => {
+    item.addEventListener("mouseover", ()=>{
+      item.classList.add("hover-square");
+      let randomColor = Math.floor(Math.random()*16777215).toString(16);
+      item.style.backgroundColor = `#${randomColor}`;
+    })
+
+  });
 }
 
-//permament hover
-sqr.forEach((item) => {
-  item.addEventListener("mouseover", ()=>{
-    item.classList.add("hover-square");
-  })
+const reset = document.querySelector(".reset");
+reset.addEventListener("click", () => {
+  body.removeChild(container);
+  canvas();
+})
 
-});
+
+canvas();
